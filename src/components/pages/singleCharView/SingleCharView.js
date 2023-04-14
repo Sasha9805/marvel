8260385f900e4eb0787b230b8fc7561d55fb03df
Sasha <pimenovs33@gmail.com>
2023-04-14 18:03:47 +0300

    Added react-helmet

--- src/components/pages/singleCharView/SingleCharView.js
+++ src/components/pages/singleCharView/SingleCharView.js
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import "./singleCharView.scss";
 
 const SingleCharView = ({ data }) => {
@@ -5,6 +7,13 @@ const SingleCharView = ({ data }) => {
 
     return (
         <div className="single-char">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${title} page`}
+                />
+                <title>{title}</title>
+            </Helmet>
             <img src={thumbnail} alt={title} className="single-char__img"/>
             <div className="single-char__info">
                 <h2 className="single-char__name">{title}</h2>
